9ef020ff04158ffb94230a6324d2294226bd2bca
whittlialleman <alle.alleman@gmail.com>
2023-07-25 11:48:45 -0600

    Finished video 22

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { MatInputModule } from '@angular/material/input'; //Input is different t
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +27,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{
+    posts = [
+        {title: 'First Post', content: 'This is the First\'s post\s content'},
+        {title: 'Second Post', content: 'This is the Second\'s post\s content'},
+        {title: 'Third Post', content: 'This is the Third\'s post\s content'},
+    ];
+}
