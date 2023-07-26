3025f908653baa8408404a973d8684b59a9bf267
whittlialleman <alle.alleman@gmail.com>
2023-07-26 11:14:35 -0600

    Finished video 28

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,22 +1,32 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent{
+export class PostListComponent implements OnInit, OnDestroy {
     // posts = [
     //     // {title: 'First Post', content: 'This is the First\'s post\s content'},
     //     // {title: 'Second Post', content: 'This is the Second\'s post\s content'},
     //     // {title: 'Third Post', content: 'This is the Third\'s post\s content'},
     // ];
-    @Input() posts: Post[] = [];
-   
+    posts: Post[] = [];
+   private postsSub: Subscription;
 
-    constructor(public postsService: PostsService) {
+    constructor(public postsService: PostsService) {}
 
+    ngOnInit() {
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+            this.posts = posts;
+        });
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
     }
 }
