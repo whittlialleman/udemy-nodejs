4e9f0f8faa057ef83cf2f2decfa2d8a03614190e
whittlialleman <alle.alleman@gmail.com>
2023-07-26 10:08:04 -0600

    Finished video 24

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-post-list',
@@ -6,9 +6,10 @@ import { Component } from "@angular/core";
     styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent{
-    posts = [
-        // {title: 'First Post', content: 'This is the First\'s post\s content'},
-        // {title: 'Second Post', content: 'This is the Second\'s post\s content'},
-        // {title: 'Third Post', content: 'This is the Third\'s post\s content'},
-    ];
+    // posts = [
+    //     // {title: 'First Post', content: 'This is the First\'s post\s content'},
+    //     // {title: 'Second Post', content: 'This is the Second\'s post\s content'},
+    //     // {title: 'Third Post', content: 'This is the Third\'s post\s content'},
+    // ];
+    @Input() posts = [];
 }
