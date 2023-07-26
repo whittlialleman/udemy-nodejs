8de0b17dd6430820541c46620ecd5ba97b8b6bda
whittlialleman <alle.alleman@gmail.com>
2023-07-26 10:56:11 -0600

    Finished video 26

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Post } from "../post.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-post-create',
@@ -11,8 +12,12 @@ export class PostCreateComponent {
     enteredContent = '';
     @Output() postCreated = new EventEmitter<Post>();
 
-    onAddPost() {
-        const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+    onAddPost(form: NgForm) {
+        if(form.invalid) {
+            return;
+        }
+        
+        const post: Post = {title: form.value.title, content: form.value.content};
         this.postCreated.emit(post);
     }
 }
