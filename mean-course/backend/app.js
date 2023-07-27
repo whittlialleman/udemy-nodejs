e3dcd4644aaca0022fa675c538d0eef3180fd32a
whittlialleman <alle.alleman@gmail.com>
2023-07-27 08:10:47 -0600

    Finished video 38

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    //console.log('First middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    //res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        { id: 'asdfsd', title: 'First server-side post', content: 'This is coming from the server'},
+        { id: 'ytujygdghh', title: 'Second server-side post', content: 'This is coming from the server!'},
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
