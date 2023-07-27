0c8a311862b07a5410733747de02fcaee81474f9
whittlialleman <alle.alleman@gmail.com>
2023-07-27 09:24:42 -0600

    Finished video 52

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post')
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,7 +16,10 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully!'
