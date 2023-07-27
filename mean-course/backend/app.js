655efd5b4bb13b33426bc4875b77beb9e7eae796
whittlialleman <alle.alleman@gmail.com>
2023-07-27 08:47:28 -0600

    Finished video 42

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlendcoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
