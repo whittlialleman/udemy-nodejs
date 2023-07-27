76389c5a53b4a5b13d29a9fb06c0274fffde5e2a
whittlialleman <alle.alleman@gmail.com>
2023-07-27 08:43:10 -0600

    Finished video 41

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlendcoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully!'
+    });
 });
 
 app.use('/api/posts', (req, res, next) => {
