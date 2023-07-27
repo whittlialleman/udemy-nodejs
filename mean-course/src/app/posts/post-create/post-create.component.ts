24b0105fd0d58f0ca763944852891ce1cd0d3381
whittlialleman <alle.alleman@gmail.com>
2023-07-27 11:15:47 -0600

    Finished video 67

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -12,9 +12,9 @@ import { Post } from "../post.model";
 export class PostCreateComponent implements OnInit {
     enteredTitle = '';
     enteredContent = '';
+    post: Post;
     private mode = 'create';
     private postId: string;
-    private post: Post;
 
     constructor(public postsService: PostsService, public route: ActivatedRoute) {}
 
@@ -31,11 +31,15 @@ export class PostCreateComponent implements OnInit {
         });
     }
 
-    onAddPost(form: NgForm) {
+    onSavePost(form: NgForm) {
         if(form.invalid) {
             return;
         }
-        this.postsService.addPost(form.value.title, form.value.content);
+        if (this.mode === 'create') {
+            this.postsService.addPost(form.value.title, form.value.content);
+        } else {
+            this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+        }
         form.resetForm();
     }
 }
