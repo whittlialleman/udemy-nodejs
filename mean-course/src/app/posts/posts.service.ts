e8f594f0c544b9d942e450f6969ea7358d94af2f
whittlialleman <alle.alleman@gmail.com>
2023-07-27 08:18:44 -0600

    Finished video 39

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
+    constructor(private http: HttpClient) {}
+
     getPosts() {
-        return this.posts;
+        this.http.get<{message: string, posts: Post[]}>('http://localhost:300/api/posts')
+        .subscribe((postData) => {
+            this.posts = postData.posts;
+            this.postsUpdated.next([...this.posts]);
+        });
     }
 
     getPostUpdateListener() {
@@ -16,7 +23,7 @@ export class PostsService {
     }
 
     addPost(title: string, content: string) {
-        const post: Post = {title: title, content: content};
+        const post: Post = {id: null, title: title, content: content};
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
     }
