77fc96ced2d660a6e2fbf9e59a5aeb704388d52f
whittlialleman <alle.alleman@gmail.com>
2023-07-27 10:03:07 -0600

    Finished video 55

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -35,14 +35,13 @@ app.post('/api/posts', (req, res, next) => {
     });
 });
 
-app.use('/api/posts', (req, res, next) => {
-    const posts = [
-        { id: 'asdfsd', title: 'First server-side post', content: 'This is coming from the server'},
-        { id: 'ytujygdghh', title: 'Second server-side post', content: 'This is coming from the server!'},
-    ];
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+app.get('/api/posts', (req, res, next) => {
+    Post.find()
+    .then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts: documents
+        });
     });
 });
 
