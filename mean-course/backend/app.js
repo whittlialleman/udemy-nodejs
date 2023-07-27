d2be5dfcf7b69a281dbc78dc857b9b6e0332b10c
whittlialleman <alle.alleman@gmail.com>
2023-07-27 08:05:57 -0600

    Finished video 37

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('First middleware');
+    //console.log('First middleware');
     next();
 });
 
 app.use((req, res, next) => {
-    res.send('Hello from express');
+    //res.send('Hello from express');
 });
 
 module.exports = app;
