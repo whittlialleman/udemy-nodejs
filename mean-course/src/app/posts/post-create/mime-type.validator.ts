ad39e31e15484736c0e1db5fc4f895b3db64a6c5
whittlialleman <alle.alleman@gmail.com>
2023-07-31 16:06:23 -0600

    Finished video 83

--- mean-course/src/app/posts/post-create/mime-type.validator.ts
+++ mean-course/src/app/posts/post-create/mime-type.validator.ts
@@ -6,7 +6,7 @@ export const mimeType = (control: AbstractControl): Promise<{[key: string]: any}
     const fileReader = new FileReader();
     const frObs = Observable.create((observer: Observer<{[key: string]: any}>) => {
         fileReader.addEventListener('loadend', () => {
-            const arr = new Uint8Array((fileReader.result as ArrayBuffer).subarray(0, 4));
+            const arr = new Uint8Array(fileReader.result as ArrayBuffer).subarray(0, 4);
             let header = '';
             let isValid = false;
             for (let i = 0; i < arr.length; i++) {
