c0be0a43bb0b2a9a5262e93ee4a8978a12372f23
whittlialleman <alle.alleman@gmail.com>
2023-07-31 14:17:48 -0600

    Finished video 75

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -23,7 +23,8 @@ export class PostCreateComponent implements OnInit {
     ngOnInit() {
         this.form = new FormGroup({
             'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-            'content': new FormControl(null, {validators: [Validators.required]})
+            'content': new FormControl(null, {validators: [Validators.required]}),
+            'image': new FormControl(null, {validators: [Validators.required]})
         });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if (paramMap.has('postId')) {
@@ -40,6 +41,12 @@ export class PostCreateComponent implements OnInit {
                 this.postId = null;
             }
         });
+    }
+
+    onImagePicked(event: Event) {
+        const file = (event.target as HTMLInputElement).files[0];
+        this.form.patchValue({image: file});
+        this.form.get('image').updateValueAndValidity();
     }
 
     onSavePost() {
