e82c22c801539ff9fb9c4c91352c9f8723ebec87
whittlialleman <alle.alleman@gmail.com>
2023-07-31 15:30:19 -0600

    Finished videos 78 and 79

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { PostsService } from "../posts.service";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Post } from "../post.model";
+import { mimeType } from "./mime-type.validator";
 
 @Component({
     selector: 'app-post-create',
@@ -25,7 +26,7 @@ export class PostCreateComponent implements OnInit {
         this.form = new FormGroup({
             'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
             'content': new FormControl(null, {validators: [Validators.required]}),
-            'image': new FormControl(null, {validators: [Validators.required]})
+            'image': new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
         });
         this.route.paramMap.subscribe((paramMap: ParamMap) => {
             if (paramMap.has('postId')) {
