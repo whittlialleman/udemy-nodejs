6356e7eb6fce2b8cfa2e684f9e0c4c8b304e73c2
whittlialleman <alle.alleman@gmail.com>
2023-07-31 15:49:42 -0600

    Finished video 81

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -19,7 +19,8 @@ export class PostsService {
                 return {
                     title: post.title,
                     content: post.content,
-                    id: post._id
+                    id: post._id,
+                    imagePath: post.imagePath
                 };
             });
         }))
@@ -43,9 +44,9 @@ export class PostsService {
         postData.append('content', content);
         postData.append('image', image, title);
         this.http
-        .post<{message: string, postId: string}>('http://localhost:3000/api/posts', postData)
+        .post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
         .subscribe((responseData) => {
-            const post: Post = {id: responseData.postId, title: title, content: content};
+            const post: Post = {id: responseData.post.id, title: title, content: content, imagePath: responseData.post.imagePath};
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
             this.router.navigate(['/']);
@@ -53,7 +54,7 @@ export class PostsService {
     }
 
     updatePost(id: string, title: string, content: string) {
-        const post: Post = {id: id, title: title, content: content};
+        const post: Post = {id: id, title: title, content: content, imagePath: null};
         this.http.put('http://localhost:3000/api/posts/' + id, post)
         .subscribe(response => {
             const updatedPosts = [...this.posts];
