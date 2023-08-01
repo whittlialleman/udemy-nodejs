9261d68cddce0d92027985a48d0e0c38edf1ead2
whittlialleman <alle.alleman@gmail.com>
2023-08-01 13:04:21 -0600

    Finished video 89

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -63,8 +63,14 @@ router.put('/:id', multer({storage: storage}).single('image'), (req, res, next) 
 });
 
 router.get('', (req, res, next) => {
-    Post.find()
-    .then(documents => {
+    const pageSize = +req.query.pagesize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if (pageSize && currentPage) {
+        postQuery.skip(pageSize * (currentPage + 1))
+        .limit(pageSize);
+    }
+    postQuery.then(documents => {
         res.status(200).json({
             message: 'Posts fetched successfully!',
             posts: documents
