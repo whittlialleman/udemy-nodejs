fcdd09ac77eac013b89b4574893c8f5c11e22de0
whittlialleman <alle.alleman@gmail.com>
2023-08-04 10:17:29 -0600

    Finished video 108

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -43,4 +43,10 @@ export class AuthService {
             }
         });
     }
+
+    logout() {
+        this.token = null;
+        this.isAuthenticated = false;
+        this.authStatusListener.next(false);
+    }
 }
