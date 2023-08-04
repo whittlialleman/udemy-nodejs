4103743438b4f5c8003e0105ed1d89d2c0423e2f
whittlialleman <alle.alleman@gmail.com>
2023-08-04 08:05:07 -0600

    Finished video 98

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user')
 
 const app = express();
 
@@ -27,5 +28,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
