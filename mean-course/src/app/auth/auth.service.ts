7305b8a2910dfdd36bbf0cf763ea8e60f91b0808
whittlialleman <alle.alleman@gmail.com>
2023-08-04 10:36:53 -0600

    Finished video 111

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { Router } from "@angular/router";
 export class AuthService {
     private isAuthenticated = false;
     private token: string;
+    private tokenTimer: any;
     private authStatusListener = new Subject<boolean>();
 
     constructor(private http: HttpClient, private router: Router) {}
@@ -34,11 +35,15 @@ export class AuthService {
 
     login(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+        this.http.post<{token: string, expiresIn: number}>('http://localhost:3000/api/user/login', authData)
         .subscribe(response => {
             const token = response.token;
             this.token = token;
             if(token) {
+                const expiresInDuration = response.expiresIn;
+                this.tokenTimer = setTimeout(() => {
+                    this.logout();
+                }, expiresInDuration * 1000)
                 this.isAuthenticated = true;
                 this.authStatusListener.next(true);
                 this.router.navigate(['/']);
@@ -50,6 +55,7 @@ export class AuthService {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
+        clearTimeout(this.tokenTimer);
         this.router.navigate(['/']);
     }
 }
