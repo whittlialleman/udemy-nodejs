f6bd936e6acb28656a092b4859b61e6cf5242ce1
whittlialleman <alle.alleman@gmail.com>
2023-08-04 08:58:30 -0600

    Finished video 102

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -21,6 +22,35 @@ router.post('/signup', (req, res, next) => {
             res.status(500).json({
                 error: err
             });
+        });
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+        if (!result) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        const token = jwt.sign(
+            {email: user.email, userId: user._id}, 
+            'secret_this_should_be_longer', 
+            {expiresIn: '1h'}
+        );
+    })
+    .catch(err => {
+        return res.status(401).json({
+            message: 'Auth failed'
         });
     });
 });
