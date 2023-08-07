6ffafd7500444f7e6a2e3c924e31ca99cba1c884
whittlialleman <alle.alleman@gmail.com>
2023-08-07 15:04:29 -0600

    Finished video 139

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./auth-data.model";
 import { Subject } from "rxjs";
 import { Router } from "@angular/router";
+import { environment } from "src/environments/environment";
+
+const BACKEND_URL = environment.apiUrl + '/user/';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -32,7 +35,7 @@ export class AuthService {
 
     createUser(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        this.http.post('http://localhost:3000/api/user/signup', authData)
+        this.http.post(BACKEND_URL + '/signup', authData)
         .subscribe(() => {
             this.router.navigate['/'];
         }, error => {
@@ -42,7 +45,7 @@ export class AuthService {
 
     login(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        this.http.post<{token: string, expiresIn: number, userId: string}>('http://localhost:3000/api/user/login', authData)
+        this.http.post<{token: string, expiresIn: number, userId: string}>(BACKEND_URL + '/login', authData)
         .subscribe(response => {
             const token = response.token;
             this.token = token;
--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -4,6 +4,9 @@ import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { environment } from "src/environments/environment";
+
+const BACKEND_URL = environment.apiUrl + '/posts/';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -14,7 +17,7 @@ export class PostsService {
 
     getPosts(postsPerPage: number, currentPage: number) {
         const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
-        this.http.get<{message: string, posts: any, maxPosts: number}>('http://localhost:3000/api/posts' + queryParams)
+        this.http.get<{message: string, posts: any, maxPosts: number}>(BACKEND_URL + queryParams)
         .pipe(map(postData => {
             return {posts: postData.posts.map(post => {
                 return {
@@ -38,7 +41,7 @@ export class PostsService {
     }
 
     getPost(id: string) {
-        return this.http.get<{_id: string; title: string, content: string, imagePath: string, creator: string}>('http://localhost:3000/api/posts/' + id);
+        return this.http.get<{_id: string; title: string, content: string, imagePath: string, creator: string}>(BACKEND_URL + id);
     }
 
     addPost(title: string, content: string, image: File) {
@@ -47,7 +50,7 @@ export class PostsService {
         postData.append('content', content);
         postData.append('image', image, title);
         this.http
-        .post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
+        .post<{message: string, post: Post}>(BACKEND_URL, postData)
         .subscribe((responseData) => {
             this.router.navigate(['/']);
         });
@@ -64,13 +67,13 @@ export class PostsService {
         } else {
             postData = { id: id, title: title, content: content, imagePath: image, creator: null };
         }
-        this.http.put('http://localhost:3000/api/posts/' + id, postData)
+        this.http.put(BACKEND_URL + id, postData)
         .subscribe(response => {
             this.router.navigate(['/']);
         });
     }
 
     deletePost(postId: string) {
-        return this.http.delete('http://localhost:3000/api/posts/' + postId);
+        return this.http.delete(BACKEND_URL + postId);
     }
 }
