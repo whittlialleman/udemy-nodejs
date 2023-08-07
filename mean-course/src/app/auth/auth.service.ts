16d327c3c83bf305e36de948143407a92c006f5a
whittlialleman <alle.alleman@gmail.com>
2023-08-07 13:18:38 -0600

    Finished video 123

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -33,8 +33,10 @@ export class AuthService {
     createUser(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
         this.http.post('http://localhost:3000/api/user/signup', authData)
-        .subscribe(response => {
-            console.log(response);
+        .subscribe(() => {
+            this.router.navigate['/'];
+        }, error => {
+            this.authStatusListener.next(false);
         });
     }
 
@@ -55,6 +57,8 @@ export class AuthService {
                 this.saveAuthData(token, expirationDate, this.userId);
                 this.router.navigate(['/']);
             }
+        }, error => {
+            this.authStatusListener.next(false);
         });
     }
 
