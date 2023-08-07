f96b002843dfd3d86a5c360516e9db87d575262a
whittlialleman <alle.alleman@gmail.com>
2023-08-07 14:54:52 -0600

    Finished video 137

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { ErrorInterceptor } from './error.interceptor';
 import { ErrorComponent } from './error/error.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/posts.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -26,8 +25,7 @@ import { AuthModule } from './auth/auth.module';
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
-    PostsModule,
-    AuthModule
+    PostsModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}, 
